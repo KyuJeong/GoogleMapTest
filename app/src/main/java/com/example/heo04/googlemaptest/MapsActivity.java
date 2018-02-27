1da05cd2c72c1d3a21c5b8b123a818aa232e7fb6
KyuJeong <heo0414@naver.com>
2018-02-27 22:22:43 +0900

    버튼으로 실내 / 실외 구분해서 지도상에 자취 그리기

--- app/src/main/java/com/example/heo04/googlemaptest/MapsActivity.java
+++ app/src/main/java/com/example/heo04/googlemaptest/MapsActivity.java
@@ -4,12 +4,22 @@ import android.Manifest.permission;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,24 +29,159 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
 
+  private boolean isPush;
+
+  /* Avoid counting faster than stepping */
+  private boolean pocketFlag;
+  private boolean callingFlag;
+  private boolean handHeldFlag;
+  private boolean handTypingFlag;
+
+  /* Set threshold */
+  private final double maxPocketThs = 15.0;
+  private final double minPocketThs = 11.5;
+  private final double maxCallingThs = 12.7;
+  private final double minCallingThs = 11.1;
+  private final double maxTypingThs = 13.5;
+  private final double minTypingThs = 11.0;
+  private final double maxHeldThs = 13.8;
+  private final double minHeldThs = 11.0;
+  private final double handHeldXThs = 1.0;
+  private final double handHeldZThs = 1.5;
+  private final double handTypingThs = 0.5;
+
+  private int stepCount;
+  private int compassCount;
+
+  private SensorManager mSensorManager = null;
+
+  // Using the Accelometer
+  private SensorEventListener mAccLis;
+  private Sensor mAccelometerSensor = null;
+
+  // Using the Gyroscoper
+  private SensorEventListener mGyroLis;
+  private Sensor mGgyroSensor = null;
+
+  // Using the Closesensor
+  private SensorEventListener mClsLis;
+  private Sensor mClsSensor = null;
+
+  // Using the Dirsensor
+  private SensorEventListener mDirLis;
+  private Sensor mDirSensor = null;
+
+  // compass Value
+  private int compassValue;
+
+  // To distinguish state
+  private boolean isPocket;
+  private boolean isHandHeld;
+  private boolean isHandTyping;
+  private boolean isPocketToHand;
+
+  private float distance;
+
+  // prevent abnormal count
+  private long startTime;
+  private long endTime;
+
+  // To use googlemap
   private GoogleMap mMap;
   private PolylineOptions mPolylineOptions;
   private Marker mMarker;
-  private double mLat;
-  private double mLng;
+  double mLat;
+  double mLng;
+
+  private boolean indoorFlag;
+
+  // View information
+  private int oneStepWidth;
+  private int oneStepHeight;
+
+  private int mDisplayWidth;
+  private int mDisplayHeight;
+  private int startWidth;
+  private int startHeight;
+
+  private int tempCount = 0;
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mSensorManager.registerListener(this, mClsSensor, SensorManager.SENSOR_DELAY_FASTEST);
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    isPush = true;
+    pocketFlag = true;
+    handHeldFlag = true;
+    handTypingFlag = true;
+    isPocket = false;
+    stepCount = 0;
+    compassCount = 0;
+
+    //Using the Sensors
+    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+    //Using the Accelometer
+    mAccelometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    //Using the Gyroscoper
+    mGgyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+    //Using the DirSensor
+    mDirSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+    //Using the Closesensor
+    mClsSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    mClsLis = new SensorEventListener() {
+      @Override
+      public void onSensorChanged(SensorEvent event) {
+        float[] v = event.values;
+        distance = v[0];
+        //Log.e("DISTANCE", String.valueOf(distance));
+
+        if (distance < 5.0) {
+          startTime = 0;
+          isPocket = true;
+        } else {
+          isPocket = false;
+        }
+      }
+
+      @Override
+      public void onAccuracyChanged(Sensor sensor, int i) {
+
+      }
+    };
+
     mPolylineOptions = new PolylineOptions();
+
+    stepCount = 0;
 
     setContentView(R.layout.activity_maps);
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
         .findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+
+    DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+
+    mDisplayWidth = dm.widthPixels;
+    mDisplayHeight =  dm.heightPixels;
+
+    startWidth = mDisplayWidth / 2;
+    startHeight = mDisplayHeight / 2 - 36;
+
+    oneStepWidth = mDisplayWidth / 27;
+    oneStepHeight = mDisplayWidth / 67;
+
+    indoorFlag = false;
 
     final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -54,26 +199,278 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
       return;
     }
     lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
+
+    findViewById(R.id.btn_log).setOnClickListener(new Button.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+        //TODO : 버튼 눌렀을 때 말고 빛 센서로 실내 실외 구분
+
+        if (isPush) {
+          mDirLis = new mDirectionListener();
+          mAccLis = new AccelometerListener();
+          mGyroLis = new GyroscopeListener();
+          mSensorManager
+              .registerListener(mAccLis, mAccelometerSensor, SensorManager.SENSOR_DELAY_UI);
+          mSensorManager
+              .registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+          mSensorManager
+              .registerListener(mClsLis, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),
+                  SensorManager.SENSOR_DELAY_FASTEST);
+          mSensorManager.registerListener(mDirLis, mDirSensor, SensorManager.SENSOR_DELAY_NORMAL);
+          isPush = false;
+          indoorFlag = true;
+        } else {
+          mSensorManager.unregisterListener(mAccLis);
+          mSensorManager.unregisterListener(mClsLis);
+          mSensorManager.unregisterListener(mDirLis);
+          isPush = true;
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    Log.e("LOG", "onPause()");
+    mSensorManager.unregisterListener(mAccLis);
+    mSensorManager.unregisterListener(mClsLis);
+    mSensorManager.unregisterListener(mDirLis);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    Log.e("LOG", "onDestroy()");
+    mSensorManager.unregisterListener(mAccLis);
+    mSensorManager.unregisterListener(mClsLis);
+    mSensorManager.unregisterListener(mDirLis);
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent sensorEvent) {
+
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int i) {
+
+  }
+
+  private class AccelometerListener implements SensorEventListener {
+
+    Point point;
+    LatLng latlng;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+      if (indoorFlag) {
+        if (startTime == 0) {
+          startTime = event.timestamp;
+        } else {
+          endTime = event.timestamp;
+        }
+
+        if (endTime - startTime > 1700000000) {
+          isPocketToHand = true;
+        } else {
+          isPocketToHand = false;
+        }
+        double accX = event.values[0];
+        double accY = event.values[1];
+        double accZ = event.values[2];
+
+        double tmp = (accX * accX) + (accY * accY) + (accZ * accZ);
+        final double E = Math.sqrt(tmp);
+
+        if(stepCount == 0) {
+          point = new Point(startWidth, startHeight);
+          latlng = new LatLng(mLat, mLng);
+          mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 21));
+          //TODO : ZOOM 버튼 만들고 버튼으로 줌 조절
+        }
+
+        /** In the pocket **/
+        if (isPocket) {
+          if (E > minPocketThs && E < maxPocketThs && pocketFlag && isPocketToHand) {
+            stepCount++;
+            pocketFlag = false;
+
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+              public void run() {
+                pocketFlag = true;
+              }
+            }, 400);
+          }
+
+          /** Calling **/
+          else if (E > minCallingThs && E < maxCallingThs && callingFlag && isPocketToHand) {
+            stepCount++;
+            callingFlag = false;
+
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+              public void run() {
+                callingFlag = true;
+              }
+            }, 400);
+          }
+        } else {
+          /** Walking with typing **/
+          if (E > minTypingThs && E < maxTypingThs && isHandTyping && handTypingFlag
+              && isPocketToHand) {
+            stepCount++;
+            isHandTyping = false;
+            handTypingFlag = false;
+
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+              public void run() {
+                handTypingFlag = true;
+              }
+            }, 400);
+          }
+          /** Hand held working **/
+          else if (E > minHeldThs && E < maxHeldThs && isHandHeld && handHeldFlag) {
+            stepCount++;
+
+            isHandHeld = false;
+            handHeldFlag = false;
+
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+              public void run() {
+                handHeldFlag = true;
+              }
+            }, 400);
+          }
+
+          if (stepCount == tempCount) {
+            if (mMarker != null) {
+              mMarker.remove();
+            }
+
+            if (tempCount > 0) {
+              latlng = mMap.getProjection().fromScreenLocation(point);
+            }
+
+            /** 걸었을 때 방위에 맞춰서 계산 **/
+            if (stepCount > 1) {
+              point.y = startHeight;
+
+              double widthAngle = Math.sin(Math.toRadians(compassValue));
+              double heightAngle = Math.cos(Math.toRadians(compassValue));
+
+              double doubleWidth = startWidth + oneStepWidth * widthAngle;
+              double doubleHeight = startHeight - oneStepHeight * heightAngle;
+
+              point.x = (int) doubleWidth;
+              point.y = (int) doubleHeight;
+            }
+
+            if (indoorFlag) {
+              Log.e("위경도 : ", "위도 : " + String.valueOf(latlng.latitude) + " 경도 : " + String
+                  .valueOf(latlng.longitude));
+              Log.e("포인트 : ", "X : " + String.valueOf(point.x) + " Y : " + String.valueOf(point.y));
+              Log.e("Step ", String.valueOf(stepCount));
+              // TODO : 화면에 몇 걸음 걸었고 몇 미터 걸었는지
+            }
+
+            mMarker = mMap
+                .addMarker(new MarkerOptions().position(latlng).title("current location"));
+
+            mMap.addPolyline(mPolylineOptions.add(latlng).color(Color.RED).width(5));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 21));
+
+            tempCount++;
+          }
+        }
+      }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+  }
+
+  private class GyroscopeListener implements SensorEventListener {
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+      /* receives the angular velocity of each axis. */
+      double gyroX = event.values[0];
+      double gyroY = event.values[1];
+      double gyroZ = event.values[2];
+
+      /* detect gyroZ motion when walking with hand */
+      if (Math.abs(gyroZ) > handHeldZThs) {
+        isHandHeld = true;
+      }
+
+      /* if gyroX moves a lot, it is not time to walking with hand */
+      if (Math.abs(gyroX) > handHeldXThs) {
+        isHandHeld = false;
+      }
+
+      /* detect few motion when walking while typing */
+      if (Math.abs(gyroX) < handTypingThs && Math.abs(gyroY) < handTypingThs
+          && Math.abs(gyroZ) < handTypingThs) {
+        isHandTyping = true;
+      } else {
+        isHandTyping = false;
+      }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+  }
+
+  private class mDirectionListener implements SensorEventListener {
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+      if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+        compassValue = (int) event.values[0];
+        Log.e("compassValue : ", String.valueOf(compassValue));
+      }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
   }
 
   private final LocationListener mLocationListener = new LocationListener() {
+
+
     @Override
     public void onLocationChanged(Location location) {
-      mLat = location.getLatitude();
-      mLng = location.getLongitude();
-      double altitude = location.getAltitude();
+      if (!indoorFlag) {
+        mLat = location.getLatitude();
+        mLng = location.getLongitude();
 
-      if(mMarker != null)
-        mMarker.remove();
+        if (mMarker != null) {
+          mMarker.remove();
+        }
 
-      LatLng latlng = new LatLng(mLat,mLng);
+        LatLng latlng = new LatLng(mLat, mLng);
 
-      mMarker = mMap.addMarker(new MarkerOptions().position(latlng).title("current location"));
+        mMarker = mMap.addMarker(new MarkerOptions().position(latlng).title("current location"));
 
-      mMap.addPolyline(mPolylineOptions.add(latlng).color(Color.RED).width(5));
+        mMap.addPolyline(mPolylineOptions.add(latlng).color(Color.RED).width(5));
 
-      mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 20));
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 21));
+      }
     }
 
     @Override
@@ -96,10 +493,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
 
+    mMap.getUiSettings().setZoomGesturesEnabled(false);
+    mMap.getUiSettings().setScrollGesturesEnabled(false);
+    mMap.getUiSettings().setRotateGesturesEnabled(false);
+    mMap.getUiSettings().setCompassEnabled(true);
+
     // Add a marker in Sydney, Australia, and move the camera.
    /* LatLng sydney = new LatLng(mLat, mLng);
     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
   }
+
 
 }
